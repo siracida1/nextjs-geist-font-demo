8b5e9aad283b9cb29769b40f95ae706f99c51fbb
siracida1 <71739509+siracida1@users.noreply.github.com>
2025-07-25 01:14:21 +0200

    Add .next/static/chunks/src_app_property_new_page_tsx_a7f20406._.js 1753398861330

---
+++ .next/static/chunks/src_app_property_new_page_tsx_a7f20406._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_d1136e0f._.js",
+  "static/chunks/node_modules_2c8b9535._.js"
+],
+    source: "dynamic"
+});
